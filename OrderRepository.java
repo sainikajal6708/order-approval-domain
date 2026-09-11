20359628316bb11e8af97973879d45ba9d75199c
sainikajal6708 <sainikajal6708@gmail.com>
2026-09-11 14:00:08 +0530

    Delete OrderRepository.java

--- OrderRepository.java
+++
@@ -1,4 +0,0 @@
-package com.rabtech.order.port;
-import com.rabtech.order.domain.*;
-import java.util.Optional;
-public interface OrderRepository{ void save(Order o); Optional<Order> findById(OrderId id); }
