556a7cdd49beb0a90b175a4cbc7547478b49af1d
sainikajal6708 <sainikajal6708@gmail.com>
2026-09-11 13:59:03 +0530

    Create OrderRepository.java

---
+++ OrderRepository.java
@@ -0,0 +1,4 @@
+package com.rabtech.order.port;
+import com.rabtech.order.domain.*;
+import java.util.Optional;
+public interface OrderRepository{ void save(Order o); Optional<Order> findById(OrderId id); }
